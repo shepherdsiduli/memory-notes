692ce033c3af08ef8a71e58945910a20461a8123
Shepherd Siduli <shepherdsiduli@gmail.com>
2020-06-29 00:07:35 +0200

    - added word count feature

--- app/src/main/java/com/shepherd/memorynotes/framework/NoteViewModel.kt
+++ app/src/main/java/com/shepherd/memorynotes/framework/NoteViewModel.kt
@@ -4,11 +4,6 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.shepherd.core.data.Note
-import com.shepherd.core.repository.NoteRepository
-import com.shepherd.core.usecase.AddNote
-import com.shepherd.core.usecase.GetAllNotes
-import com.shepherd.core.usecase.GetNote
-import com.shepherd.core.usecase.RemoveNote
 import com.shepherd.memorynotes.framework.di.ApplicationModule
 import com.shepherd.memorynotes.framework.di.DaggerViewModelComponent
 import kotlinx.coroutines.CoroutineScope
---
+++ core/src/main/java/com/shepherd/core/usecase/GetWordCount.kt
@@ -0,0 +1,14 @@
+package com.shepherd.core.usecase
+
+import com.shepherd.core.data.Note
+
+class GetWordCount {
+    operator fun invoke(note: Note) = getCount(note.title) + getCount(note.content)
+
+    private fun getCount(str: String) =
+        str.split(" ", "\n")
+            .filter {
+                it.contains(Regex(".*[a-zA-Z].*"))
+            }
+            .count()
+}
